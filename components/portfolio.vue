6bb01b750a1d9d7103b0177e7650aae7d37bf998
RahulS77 <rahulshah19967@gmail.com>
2022-04-02 15:55:42 +0700

    init

---
+++ components/portfolio.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <div id="component-profile">
+      Hello
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello World
+    <portfolio />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Portfolio: () => import('/components/portfolio')
+  }
+}
+</script>
